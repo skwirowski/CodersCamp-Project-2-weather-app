080092f5cc201349ac6ac42b7aed7b36fdc8182d
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-11-22 15:02:53 +0100

    actual weather display

---
+++ src/javascript/actualWeather/actualWeather.js
@@ -0,0 +1,14 @@
+
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const createWeatherHTML = currentDay => {
+  console.log(currentDay);
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = /*html*/ `<h2>${weekDays[new Date().getDay()]}</h2>
+		<h2>Temperature: ${currentDay.main.temp}&deg;C</h2>
+		<h2>Condition: ${currentDay.weather[0].description}</h2>
+    <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
+  document.body.appendChild(newDiv);
+};
+
+export default createWeatherHTML
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,29 +1,33 @@
-import externalFunction from './exampleScript';
-import { anotherExternalFunction } from './anotherExampleScript';
+// import externalFunction from './exampleScript';
+// import { anotherExternalFunction } from './anotherExampleScript';
 import '../less/styles.less';
 import todayByName from './utility/todayByName';
 import todayByLocalization from './utility/todayByLocalization';
 import forecastByName from './utility/forecastByName';
 import forecastByLocalization from './utility/forecastByLocalization';
+import createWeatherHTML from './actualWeather/actualWeather';
 
-const something = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const something = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-const funct = () => console.log('spread operator works', [...something]);
+// const funct = () => console.log('spread operator works', [...something]);
 
-const newFunction = async () => {
-  const someData = await fetch('https://randomuser.me/api/?results=2');
-  const resolvedData = await someData.json();
-  const readyData = resolvedData;
-  console.log('async await function works fine;', readyData.results);
-};
+// const newFunction = async () => {
+//   const someData = await fetch('https://randomuser.me/api/?results=2');
+//   const resolvedData = await someData.json();
+//   const readyData = resolvedData;
+//   console.log('async await function works fine;', readyData.results);
+// };
 
-funct();
-newFunction();
-externalFunction();
-console.log(anotherExternalFunction());
+// funct();
+// newFunction();
+// externalFunction();
+// console.log(anotherExternalFunction());
 
 //  Pogoda na dzisiaj po nazwie miasta
-todayByName('szczecin').then(api => console.log(`Temperatura - ${api.main.temp}`));
+todayByName('szczecin').then(api => {
+  createWeatherHTML(api);
+  console.log(`Temperatura - ${api.main.temp}`);
+});
 
 //  Pogoda na dzisiaj po lokalizacji
 todayByLocalization(15, 53).then(api => console.log(`Temperatura dla ${api.name}  - ${api.main.temp}`));
