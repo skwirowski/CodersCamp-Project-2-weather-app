1297e08c01c652b4cc9267d59800a1699ae60304
webskey <jolo_92@wp.pl>
2019-11-23 10:28:34 +0100

    changed display format

--- src/javascript/anotherExampleScript.js
+++
@@ -1,6 +0,0 @@
-export function anotherExternalFunction() {
-  const arr = [1, 2, 3, 4, 5];
-
-  const newArr = arr.map(item => `${item}-stringified`);
-  return newArr;
-}
--- src/javascript/nextHoursWeather.js
+++ src/javascript/nextHoursWeather.js
@@ -1,13 +1,14 @@
+const moment = require('moment');
+
 export default async function getWeatherByName(api) {
-  const container = document.querySelector('#nextHoursWeather');
+  const container = document.querySelector('#nextHoursWeather-container');
   let htmlCode = '';
   for (let i = 0; i < 5; i++) {
-    console.log(api.list[i]);
     htmlCode += /* html */ `
-      <div style="border:1px solid black; background-color:grey; width:150px; display:inline-block;">
-      <p>Time: ${api.list[i].dt_txt}</p>
-      <p>Temperature: ${api.list[i].main.temp}&deg;C<p>
-      <img src="https://openweathermap.org/img/wn/${api.list[i].weather[0].icon}@2x.png">
+      <div class = 'nextHoursWeather-item'>
+      <p>${moment(api.list[i].dt_txt).format('ha')}</p>
+      <img src="https://openweathermap.org/img/wn/${api.list[i].weather[0].icon}@2x.png">      
+      <p>${api.list[i].main.temp}&deg;C<p>
       </div>
       `;
   }
