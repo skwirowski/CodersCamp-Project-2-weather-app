4eb6cad81afc29ec063da41603a88b1e8e7844ab
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-11-23 11:18:41 +0100

    time of sunrise and sunset added

--- src/javascript/actualWeather.js
+++ src/javascript/actualWeather.js
@@ -1,6 +1,7 @@
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const createWeatherHTML = currentDay => {
+  console.log(currentDay)
   const actualWeather = document.getElementById('actualWeather');
   actualWeather.innerHTML = /* html */ `<h2>${currentDay.name}<h2>
   <h2>${weekDays[new Date().getDay()]}, ${date()}</h2>
@@ -9,7 +10,10 @@ const createWeatherHTML = currentDay => {
     <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">
     <h3>Pressure: ${currentDay.main.pressure}hPa</h3>
     <h3>Humidity: ${currentDay.main.humidity}%</h3>
-    <h3>Wind: ${currentDay.wind.speed}m/s</h3>`;
+    <h3>Wind: ${currentDay.wind.speed}m/s</h3>
+    <h3>Sunrise: ${time(currentDay.sys.sunrise)}</h3>
+    <h3>Sunset: ${time(currentDay.sys.sunset)}</h3>`;
+
 };
 
 const date = () => {
@@ -21,6 +25,14 @@ const date = () => {
     Year += 1900;
   }
   return `${Day}-${Month}-${Year}`;
+};
+
+const time = unix => {
+  const date = new Date(unix * 1000);
+  const hours = `0${date.getHours()}`;
+  const minutes = `0${date.getMinutes()}`;
+  const formattedTime = `${hours.substr(-2)}:${minutes.substr(-2)}`;
+  return formattedTime;
 };
 
 export default createWeatherHTML;
