1296cfd10e57dc1969666df35224559ad6627ef5
Paweł Skwirowski <pskwirowski@gmail.com>
2019-11-24 18:15:39 +0100

    Add initial general styling for application and initial section Next Days Weather styles
     - added some general variables in settings.less file
     - added some general rules in styles.less file
     - added initial styling for application
     - added styling for Next Days Weather section

--- src/javascript/nextDaysWeather.js
+++ src/javascript/nextDaysWeather.js
@@ -11,9 +11,11 @@ export default function showNextDaysWeatherData(weatherDataArray) {
 
   const weatherDataStartingFromTomorrow = createWeatherDataArrayThatStartsFromTomorrow(weatherDataArray, 'dt');
   const weatherDataAtNoon = getEveryNthArrayElement(weatherDataStartingFromTomorrow, 4, 8);
+  const forecastDaysQuantity = weatherDataAtNoon.length;
 
   const nextDaysWeatherTemplate = /* html */ `
     <div class='next-days-weather'>
+      <h2 class='next-days-weather__heading'>Next ${forecastDaysQuantity} days</h2>
       ${weatherDataAtNoon
         .map(weather => {
           const { dt } = weather;
@@ -25,17 +27,34 @@ export default function showNextDaysWeatherData(weatherDataArray) {
           const dayOfMonth = getDayOfMonth(dt);
           const month = getMonth(dt);
           const iconUrl = getIconUrl(icon, '@2x');
-          const temperature = `${roundFloatToInteger(temp)}&#8451;`;
+          const temperature = `${roundFloatToInteger(temp)}&#176;C`;
           const rainVolume = weather.rain ? `${weather.rain['3h']}mm` : '-';
 
           return /* html */ `
-          <span>${dayOfWeek}</span>
-          <span>${dayOfMonth}/${month}</span>
-          <img src=${iconUrl} alt='weather icon'>
-          <span>Temperature: ${temperature}</span>
-          <span>Wind: ${speed}m/s</span>
-          <span>Rain: ${rainVolume}</span>
-        `;
+            <div class='weather-row'>
+              <div class='weather-row__item'>
+                <span class='weather-row__item--value'>${dayOfWeek}</span>
+                <span class='weather-row__item--description'>${dayOfMonth}/${month}</span>
+              </div>
+              <img
+                class='weather-row__icon'
+                src=${iconUrl}
+                alt='weather icon'
+              >
+              <div class='weather-row__item'>
+                <span class='weather-row__item--value'>${temperature}</span>
+                <span class='weather-row__item--description'>Temp</span>
+              </div>
+              <div class='weather-row__item'>
+                <span class='weather-row__item--value'>${speed}m/s</span>
+                <span class='weather-row__item--description'>Wind</span>
+              </div>
+              <div class='weather-row__item'>
+                <span class='weather-row__item--value'>${rainVolume}</span>
+                <span class='weather-row__item--description'>Rain</span>
+              </div>
+            </div>
+          `;
         })
         .join('')}
     </div>
