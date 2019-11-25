ef7c26199c7eb36ee1f0f58df1af431ae5a132ab
katarzyna-stepczynska <stepczynska.katarzyna@gmail.com>
2019-11-25 15:08:10 +0100

    Fixed function with input

--- src/javascript/searchCity.js
+++ src/javascript/searchCity.js
@@ -1,53 +1,19 @@
-/* eslint-disable import/no-unresolved */
 /* eslint-disable global-require */
-
-// import * as cities from './static/pl.json'; // it doesn't work
-
-// export default function search() {
-//   const cities = require('./static/pl.json');
-//   console.log(cities);
-//   const findMatches = (word, city) => {
-//     return city.filter(place => {
-//       const regex = new RegExp(word, 'gi');
-//       return place.city.match(regex);
-//     });
-//   };
-//   const searchInput = document.querySelector('.search');
-//   const suggestion = document.querySelector('.suggestions');
-//   function displayMatches(e) {
-//     e.preventDefault();
-//     console.log(this.value);
-//     document.getElementById('datalist').innerHTML = '';
-//     const matchArray = findMatches(this.value, cities);
-//     console.log(matchArray);
-//     const html = matchArray
-//       .map(place => {
-//         const regex = new RegExp(this.value, 'gi');
-//         const cityName = place.city.replace(regex, `<option>${this.value}</option>`);
-//         return `
-//             <option>
-//                 ${cityName}
-//             </option>
-//             `;
-//       })
-//       .join('');
-//     suggestion.innerHTML = html;
-//   }
-//   searchInput.addEventListener('keyup', displayMatches);
-// }
-
 export default function search() {
   const cities = require('./static/pl.json');
   console.log(cities);
-  const allCities = Array.from(cities).length;
+  const convertArray = cities.map(city => city.city);
+  const allCities = cities.length;
+  console.log(convertArray);
+  console.log(allCities);
   const searchInput = document.querySelector('.search');
-  function searchCity(value) {
+
+  function searchCity(event) {
     document.getElementById('datalist').innerHTML = '';
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < allCities; i++) {
-      if (cities[i].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+    for (let i = 0; i < allCities; i += 1) {
+      if (convertArray[i].toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) {
         const node = document.createElement('option');
-        const val = document.createTextNode(cities[i]);
+        const val = document.createTextNode(convertArray[i]);
         node.appendChild(val);
         document.getElementById('datalist').appendChild(node);
       }
