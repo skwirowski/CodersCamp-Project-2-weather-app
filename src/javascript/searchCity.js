693b1075dd118f44de21d906287e551ef07b189c
katarzyna-stepczynska <stepczynska.katarzyna@gmail.com>
2019-11-25 14:06:07 +0100

    Work with other function to suggestion - not finish yet.

--- src/javascript/searchCity.js
+++ src/javascript/searchCity.js
@@ -1,34 +1,57 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable global-require */
+
+// import * as cities from './static/pl.json'; // it doesn't work
+
+// export default function search() {
+//   const cities = require('./static/pl.json');
+//   console.log(cities);
+//   const findMatches = (word, city) => {
+//     return city.filter(place => {
+//       const regex = new RegExp(word, 'gi');
+//       return place.city.match(regex);
+//     });
+//   };
+//   const searchInput = document.querySelector('.search');
+//   const suggestion = document.querySelector('.suggestions');
+//   function displayMatches(e) {
+//     e.preventDefault();
+//     console.log(this.value);
+//     document.getElementById('datalist').innerHTML = '';
+//     const matchArray = findMatches(this.value, cities);
+//     console.log(matchArray);
+//     const html = matchArray
+//       .map(place => {
+//         const regex = new RegExp(this.value, 'gi');
+//         const cityName = place.city.replace(regex, `<option>${this.value}</option>`);
+//         return `
+//             <option>
+//                 ${cityName}
+//             </option>
+//             `;
+//       })
+//       .join('');
+//     suggestion.innerHTML = html;
+//   }
+//   searchInput.addEventListener('keyup', displayMatches);
+// }
+
 export default function search() {
   const cities = require('./static/pl.json');
   console.log(cities);
-  const findMatches = (word, city) => {
-    return city.filter(place => {
-      const regex = new RegExp(word, 'gi');
-      return place.city.match(regex);
-    });
-  };
+  const allCities = Array.from(cities).length;
   const searchInput = document.querySelector('.search');
-  const suggestion = document.querySelector('.suggestions');
-  function displayMatches(e) {
-    e.preventDefault();
-    console.log(this.value);
-    const matchArray = findMatches(this.value, cities);
-    console.log(matchArray);
-    const html = matchArray
-      .map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-        return `
-            <li>
-                <span class="name">${cityName}</span>
-            </li>
-            `;
-      })
-      .join('');
-    suggestion.innerHTML = html;
+  function searchCity(value) {
+    document.getElementById('datalist').innerHTML = '';
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < allCities; i++) {
+      if (cities[i].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+        const node = document.createElement('option');
+        const val = document.createTextNode(cities[i]);
+        node.appendChild(val);
+        document.getElementById('datalist').appendChild(node);
+      }
+    }
   }
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
+  searchInput.addEventListener('keyup', searchCity);
 }
