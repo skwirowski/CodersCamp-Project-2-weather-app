f7d1b3053063d540eed39455c177f6580187c659
ŁukaSz <35524559+Webskey@users.noreply.github.com>
2019-11-25 20:44:30 +0100

    Feature geolocation
    * geolocation and styling of nextHoursWeather section
    
    * amendments after merge with master

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,38 +1,25 @@
 import todayByName from './utility/todayByName';
-import todayByLocalization from './utility/todayByLocalization';
 import forecastByName from './utility/forecastByName';
-import forecastByLocalization from './utility/forecastByLocalization';
 import airQualityByName from './utility/airQualityByName';
 import showCurrentWeatherData from './actualWeather';
 import search from './searchCity';
-import nextHoursWeather from './nextHoursWeather';
+import showNextHoursWeather from './nextHoursWeather';
 import createAirQualityHTML from './actualAirQuality';
 import showNextDaysWeatherData from './nextDaysWeather';
+import getGeolocation from './showWeatherByGeolocation';
 
 import '../less/styles.less';
 
 function getData(cityName) {
-  //  Pogoda na dzisiaj po nazwie miasta
-  todayByName(cityName).then(api => {
-    showCurrentWeatherData(api);
-    console.log(`Temperatura - ${api.main.temp}`);
+  todayByName(cityName).then(api => showCurrentWeatherData(api));
+
+  forecastByName(cityName).then(data => {
+    showNextDaysWeatherData(data.list);
+    showNextHoursWeather(data);
   });
-
-  //  Pogoda na dzisiaj po lokalizacji
-  todayByLocalization(15, 53).then(api => console.log(`Temperatura dla ${api.name}  - ${api.main.temp}`));
-
-  //  Pogoda na 5 dni po nazwie miasta
-  forecastByName(cityName).then(api => {
-    nextHoursWeather(api);
-  });
-  //  Pogoda na 5 dni po lokalizacji
-  forecastByLocalization(15, 53).then(api =>
-    console.log(`Temperatura dla ${api.city.name} - ${api.list[0].main.temp} w dniu ${api.list[0].dt_txt}`)
-  );
-
-  forecastByName(cityName).then(data => showNextDaysWeatherData(data.list));
 
   airQualityByName(cityName).then(api => createAirQualityHTML(api));
 }
-
 search(getData);
+
+getGeolocation();
