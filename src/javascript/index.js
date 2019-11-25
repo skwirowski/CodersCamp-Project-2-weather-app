e297bcf9cb98d1c1d8d38fcb6096d222c937e67f
webskey <jolo_92@wp.pl>
2019-11-25 19:21:35 +0100

    amendments after merge with master

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,33 +1,21 @@
 import todayByName from './utility/todayByName';
-import todayByLocalization from './utility/todayByLocalization';
 import forecastByName from './utility/forecastByName';
-import forecastByLocalization from './utility/forecastByLocalization';
 import createWeatherHTML from './actualWeather';
 import search from './searchCity';
-import nextHoursWeather from './nextHoursWeather';
+import showNextHoursWeather from './nextHoursWeather';
 import showNextDaysWeatherData from './nextDaysWeather';
+import getGeolocation from './showWeatherByGeolocation';
 
 import '../less/styles.less';
 
 function getData(cityName) {
-  //  Pogoda na dzisiaj po nazwie miasta
-  todayByName(cityName).then(api => {
-    createWeatherHTML(api);
-    console.log(`Temperatura - ${api.main.temp}`);
+  todayByName(cityName).then(api => createWeatherHTML(api));
+
+  forecastByName(cityName).then(data => {
+    showNextDaysWeatherData(data.list);
+    showNextHoursWeather(data);
   });
-
-  //  Pogoda na dzisiaj po lokalizacji
-  todayByLocalization(15, 53).then(api => console.log(`Temperatura dla ${api.name}  - ${api.main.temp}`));
-
-  //  Pogoda na 5 dni po nazwie miasta
-  forecastByName(cityName).then(api => {
-    nextHoursWeather(api);
-  });
-  //  Pogoda na 5 dni po lokalizacji
-  forecastByLocalization(15, 53).then(api =>
-    console.log(`Temperatura dla ${api.city.name} - ${api.list[0].main.temp} w dniu ${api.list[0].dt_txt}`)
-  );
-
-  forecastByName(cityName).then(data => showNextDaysWeatherData(data.list));
 }
 search(getData);
+
+getGeolocation();
--- src/javascript/nextHoursWeather.js
+++ src/javascript/nextHoursWeather.js
@@ -3,21 +3,18 @@ import getIconUrl from './attributes/getIconUrl';
 const moment = require('moment');
 
 export default function(api) {
-  const container = document.querySelector('#next-hours-weather-container');
+  const container = document.querySelector('#next-hours-weather');
   let htmlCode = '';
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < 5; i++) {
     htmlCode += /* html */ `
       <div class = 'next-hours-weather-item'>
-      <span class="next-hours-weather-time next-hours-weather-atr">${moment(api.list[i].dt_txt).format('ha')}</span>
-      <img class="next-hours-weather-img next-hours-weather-atr" src="${getIconUrl(
-        api.list[i].weather[0].icon,
-        '@2x'
-      )}">     
-      <span class="next-hours-weather-temp next-hours-weather-atr">${Math.round(api.list[i].main.temp)}&deg;C<span>
+      <span class="next-hours-weather-time">${moment(api.list[i].dt_txt).format('ha')}</span>
+      <img class="next-hours-weather-img" src="${getIconUrl(api.list[i].weather[0].icon, '@2x')}">     
+      <span class="next-hours-weather-temp">${Math.round(api.list[i].main.temp)}&deg;C<span>
       </div>
       `;
   }
   container.innerHTML = `<h2 class='next-hours-weather-h'>Todays weather</h2>
-  <div class='next-hours-weather-container'> ${htmlCode} </div>`;
+    <div class='next-hours-weather-container'> ${htmlCode} </div>`;
 }
