f8daa2cf25c6230a0a6aa9316b4eeb67e5e8551c
Paweł Skwirowski <pskwirowski@gmail.com>
2019-11-26 14:27:41 +0100

    Adjust weather by geolocation functionality
     - moved part of the file to index.js to keep consistency in invoking main functions and importing data fetching
    functions

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -7,10 +7,12 @@ import showNextHoursWeather from './nextHoursWeather';
 import createAirQualityHTML from './actualAirQuality';
 import showNextDaysWeatherData from './nextDaysWeather';
 import getGeolocation from './showWeatherByGeolocation';
+import todayByLocalization from './utility/todayByLocalization';
+import forecastByLocalization from './utility/forecastByLocalization';
 
 import '../less/styles.less';
 
-function getData(cityName) {
+function getDataByName(cityName) {
   todayByName(cityName).then(api => showCurrentWeatherData(api));
 
   forecastByName(cityName).then(data => {
@@ -20,6 +22,14 @@ function getData(cityName) {
 
   airQualityByName(cityName).then(api => createAirQualityHTML(api));
 }
-search(getData);
+function getDataByLocation(lat, lon) {
+  todayByLocalization(lat, lon).then(api => showCurrentWeatherData(api));
 
-getGeolocation();
+  forecastByLocalization(lat, lon).then(api => {
+    showNextHoursWeather(api);
+    showNextDaysWeatherData(api.list);
+  });
+}
+
+search(getDataByName);
+getGeolocation(getDataByLocation);
