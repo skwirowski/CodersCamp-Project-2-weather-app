0dd943c608a9be637a6d3b09906ac6cd24fa91cb
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-11-26 20:20:22 +0100

    geolocation - nearest station

--- src/javascript/utility/airQualityByLocalization.js
+++ src/javascript/utility/airQualityByLocalization.js
@@ -4,7 +4,7 @@ import { AqiUrl as url } from '../attributes/apiUrl';
 // eslint-disable-next-line consistent-return
 export default async function(lat, lon) {
   try {
-    const apiFetch = await fetch(`${url}feed/${lat}/?token=${lon}${id}`);
+    const apiFetch = await fetch(`${url}feed/geo:${lat};${lon}/?token=${id}`);
     const apiJson = await apiFetch.json();
     return apiJson;
   } catch (error) {
