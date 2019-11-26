af0c668f631d20e039f4f2e91ef4a28a8afdf87a
Paweł Skwirowski <pskwirowski@gmail.com>
2019-11-26 12:07:44 +0100

    Divide webpack configuration into production and development config files
     - added webpack.common.js with settings common to both prod and dev environments
     - added webpack.dev.js with settings for development environment
     - added webpack.prod.js with settings for production environment
     - added webpack plugin creating css output file from less stylesheets in production mode

---
+++ webpack.prod.js
@@ -0,0 +1,31 @@
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+});
